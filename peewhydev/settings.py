f303d12150ebace0e86a59167e6b0a5c8e0fa6f8
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-08-17 22:51:09 +0100

    add resume

--- peewhydev/settings.py
+++ peewhydev/settings.py
@@ -133,7 +133,7 @@ EMAIL_PORT = 587
 # EMAIL_HOST_USER = #sender's email-id
 # EMAIL_HOST_PASSWORD = #password associated with above email-id
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
