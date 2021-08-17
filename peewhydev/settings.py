80c2206c8550909281a5ad34ddd722a824a3e2f8
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-08-17 22:55:31 +0100

    add resume

--- peewhydev/settings.py
+++ peewhydev/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
